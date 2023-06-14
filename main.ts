d642bd69e50baa6549fd4d09526b6f39854ed884
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-06-14 03:20:49 +0200

    13:12 Implementing a simple plugin done - https://www.youtube.com/watch?v=AgXa03ZxJ88

--- main.ts
+++ main.ts
@@ -2,25 +2,13 @@ import {Plugin} from "obsidian";
 
 export default class ExamplePlugin extends Plugin {
 	statusBarElement: HTMLSpanElement;
-	onload()
-	{
+	onload() {
 		console.log("Hey!")
 		this.statusBarElement = this.addStatusBarItem().createEl('span');
-		this.statusBarElement.textContent = "Text inside bar!"
+		this.taskbarLineCountUpdate();
 
 		this.app.workspace.on("active-leaf-change", async () => {
-			const file = this.app.workspace.getActiveFile();
-
-			if (file)
-			{
-				const content = await this.app.vault.read(file);
-				// console.log(content);
-				this.updateLineCount(content);
-			}
-			else
-			{
-				this.updateLineCount(undefined);
-			}
+			this.taskbarLineCountUpdate();
 		});
 
 
@@ -32,10 +20,25 @@ export default class ExamplePlugin extends Plugin {
 
 	private updateLineCount(fileContent?: string)
 	{
-		const count = fileContent ? fileContent.split(/\r\n|\r|\n/).length : -1;
+		const count = fileContent ? fileContent.split(/\r\n|\r|\n/).length : -2;
 
 		const linesWord = count === 1 ? "line" : "lines";
 
 		this.statusBarElement.textContent = `${count} ${linesWord}`;
+	}
+
+	private async taskbarLineCountUpdate() {
+		const file = this.app.workspace.getActiveFile();
+
+		if (file)
+		{
+			const content = await this.app.vault.read(file);
+			// console.log(content);
+			this.updateLineCount(content);
+		}
+		else
+		{
+			this.updateLineCount(undefined);
+		}
 	}
 }
